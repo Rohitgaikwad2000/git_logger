47fc24ce9239aed5a604acb84c2533c2c7480334
Rohit_Git <gaikwadrohit1428@gmail.com>
2023-12-11 23:10:13 +0530

    add loop

--- plogger.py
+++ plogger.py
@@ -533,9 +533,5 @@ logging.error("This is error")
 logging.critical("This is critical")
 
 
-logging.debug("This is debug")
-logging.info("This is info")
-logging.warning("This is warning")
-logging.error("This is error")
-logging.critical("This is critical")
-
+for i in range(1,100):
+    print(i,"Rohit gaikwad")
